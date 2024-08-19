9b25ad5aa18ff99d1959df53f1bced395ddf017d
Ruben Diaz <rubendb95@gmail.com>
2024-08-19 16:52:02 +0200

    feat(src): install dependencies for mantine

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
+import Navigation from './components/Navigation/Navigation';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <MantineProvider>
-      <>
-        <h1>Hi! i am client</h1>
-      </>
+      <Navigation />
     </MantineProvider>
   )
 }
