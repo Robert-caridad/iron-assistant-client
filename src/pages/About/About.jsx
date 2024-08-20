41bd7fe754f9901e53991ccafd758ab43665525d
Robert-caridad <robertcaridad@gmail.com>
2024-08-20 18:15:21 +0200

    feta(componets, pages): more components and pages added

--- src/pages/About/About.jsx
+++
@@ -1,8 +0,0 @@
-const About = () => {
-    return (
-        <h1>About lol</h1>
-    )
-
-}
-
-export default About
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,10 @@ import Homepage from './../pages/HomePage/HomePage'
 import PrivateRouter from './PrivateRouters'
 import SignUpPage from '../pages/SignUp/SignUpPage'
 import LoginPage from '../pages/LoginPage/LoginPages'
+import ErrorPage from '../pages/ErrorPage/Errorpage'
+import AboutUsPage from '../pages/AboutUsPage/AboutUsPage'
+import FaqPage from '../pages/FaqPage/FaqPage'
+
 const AppRoutes = () => {
     return (
         <div className="AppRoutes">
@@ -10,9 +14,9 @@ const AppRoutes = () => {
                 <Route path='/' element={<Homepage />} />
                 <Route path='/signup' element={<SignUpPage />} />
                 <Route path='/login' element={<LoginPage />} />
-                <Route path='/about' />
-                <Route path='/faq' />
-                <Route path='*' />
+                <Route path='/about' element={<AboutUsPage />} />
+                <Route path='/faq' element={<FaqPage />} />
+                <Route path='*' element={<ErrorPage />} />
 
                 <Route element={<PrivateRouter />}>
                     <Route path='/dashboard/home' />
